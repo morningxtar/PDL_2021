a61f5939e8a61b449d21269e37cdde9e7c34a10f
machadi <anwar.machadi@gmail.com>
2020-11-02 17:50:49 +0100

    create a class converter for all the methods

---
+++ modules/test_modules.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+
+class Testconverter(TestCase):
+
+    def test_transform_wikiurls_to_realurls(self):
+        self.fail()
+
+    def test_check_found_url(self):
+        self.fail()
+
+    def test_write_csv_files(self):
+        self.fail()
