fda6014eaed3cce5410c71f77351822ab09bf594
isanogo <issasanogo007@gmail.com>
2020-11-07 02:49:54 +0100

    Add tests

--- modules/TestConverterToCsv.py
+++ modules/TestConverterToCsv.py
@@ -1,5 +1,6 @@
 import unittest
 import modules.ConverterToCsv as md
+import requests
 from os import listdir
 from os.path import isfile, join, isdir
 
@@ -10,12 +11,13 @@ class TestConverterToCsv(unittest.TestCase):
         wikiurls_path = '../input/wikiurls.txt'
         with open(wikiurls_path, 'r') as f:
             urls = md.ConverterToCsv.transform_wikiurls_to_realurls(f.readlines())
-        '''for url in urls:
-            self.assertIsNone(url)
-            '''
+        dataset = ["https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Ido",
+                   "https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Interlingua",
+                   "https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Novial"]
         self.assertEqual(336, len(urls))
+        self.assertTrue(all(item in urls for item in dataset))
 
-    def test_check_found_url1(self):
+    def test_check_found_url(self):
         url_sans_tab = "https://en.wikipedia.org/wiki/Comparison_of_Axis_&_Allies_games"
         url_avec_tab = "https://en.wikipedia.org/wiki/Comparison_of_Asian_national_space_programs"
         code, result_bool = md.ConverterToCsv.check_found_url(url_sans_tab)
@@ -24,7 +26,6 @@ class TestConverterToCsv(unittest.TestCase):
         code, result_bool = md.ConverterToCsv.check_found_url(url_avec_tab)
         self.assertTrue(result_bool)
         self.assertEqual(200, code)
-            # self.assertNotEqual(md.ConverterToCsv.check_found_url(url)[1], 0)
 
     def test_get_tables_contents_from_url(self):
         urlsTab = "https://en.wikipedia.org/wiki/Comparison_between_Esperanto_and_Ido"
@@ -37,14 +38,15 @@ class TestConverterToCsv(unittest.TestCase):
         #  Get the list of all files and directories
         #  in the root directory 'path'
         for folder in listdir(monRepertoire_output):
-            # isdir Check whether
-            # the specified path is
-            # an existing directory or not
-            # self.assertTrue(isfile(folder))
-            # used to check whether
-            # the specified path is
-            # an existing regular file or not
             self.assertTrue(isfile(join(monRepertoire_output, folder)))
+
+    def test_count_csv_files(self):
+        path_output = '../output'
+        nbFiles = 0
+        for folder in listdir(path_output):
+            if isfile(join(path_output, folder)):
+                nbFiles = nbFiles + 1
+        self.assertEqual(nbFiles, md.ConverterToCsv.count_csv_files(path_output))
 
 
 if __name__ == '__main__':
