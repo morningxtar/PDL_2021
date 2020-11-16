987d7ea9aaa68e662c84e754ed18c5d4a176055b
machadi <anwar.machadi@gmail.com>
2020-11-16 16:55:16 +0100

    Update Python_VS_Java.py

--- comparison_Ejava_Epython/Python_VS_Java.py
+++ comparison_Ejava_Epython/Python_VS_Java.py
@@ -1,14 +1,13 @@
 import os
 # execution the extractors in python
 from wikiExtractor.wikiMain import extractor_python
-
 print('---------------------------------PYTHON EXTRACTOR----------------------------------------------')
 duration_python = extractor_python()
 print('----------------------------------JAVA EXTRACTORS----------------------------------------------')
 # execution the extractors in java
-# se positionner dans le rep de projet java
-os.chdir("C:/Users/anwar/IdeaProjects/PDL_1920_groupe-7")
-# généeration Jar
+# se positionner sur le répertoire du projet Extracteurs Java
+os.chdir("../../../IdeaProjects/PDL_1920_groupe-7")
+# génération et lancement Jar
 command = "mvn clean install"
 os.system(command)
 print("Duration of the extractor in python: {} s".format(duration_python))
